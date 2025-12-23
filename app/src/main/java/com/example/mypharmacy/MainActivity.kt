04424c3cfe4000e0a5dfddaa109420974da1baef
ikstami <k.mus00@yandex.ru>
2025-12-23 23:08:43 +0700

    создания репозитория

--- app/src/main/java/com/example/mypharmacy/MainActivity.kt
+++ app/src/main/java/com/example/mypharmacy/MainActivity.kt
@@ -21,7 +21,7 @@ import androidx.lifecycle.lifecycleScope
 import com.example.mypharmacy.data.model.Medicine
 import kotlinx.coroutines.launch
 import java.util.UUID
-
+//trsr
 @OptIn(ExperimentalMaterial3Api::class)
 class MainActivity : ComponentActivity() {
 
