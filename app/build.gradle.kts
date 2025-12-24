7b60bc51f14c9e88a2ab90e6473ba6a99a2e62a1
ikstami <k.mus00@yandex.ru>
2025-12-24 16:26:34 +0700

    сейв для зависимостей

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,7 +60,7 @@ dependencies {
     implementation(platform("com.google.firebase:firebase-bom:32.7.0"))
     implementation("com.google.firebase:firebase-firestore-ktx")
     implementation("androidx.navigation:navigation-compose:2.7.3")
-        //new
+        //neww
     implementation ("androidx.core:core-ktx:1.12.0")
     implementation ("androidx.activity:activity-compose:1.9.0")
     implementation ("androidx.compose.ui:ui:1.6.0")
